34b0bf381871027e6ef3c8640f0de6a5543c7f7c
Vinodh <vinodhtandrothu@gmail.com>
2023-02-18 16:48:57 -0600

    ...

--- storefront/settings/common.py
+++ storefront/settings/common.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
-import django_heroku
+#import django_heroku
 from pathlib import Path
 from datetime import timedelta
 
@@ -211,5 +211,5 @@ LOGGING = {
 }
 
 
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
 
