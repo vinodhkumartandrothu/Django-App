faaa446976b5ae5e8470bc0ec2f7d009fe249c9f
Vinodh <vinodhtandrothu@gmail.com>
2023-02-19 17:35:28 -0600

    ...

--- storefront/settings/devlop.py
+++ storefront/settings/devlop.py
@@ -4,7 +4,7 @@ from .common import *
 DEBUG = True
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-hs6j037urx6iav+7#10%-vu4l4f5@@-1_zo)oft4g7$vf2$jmp'
-
+#v
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
