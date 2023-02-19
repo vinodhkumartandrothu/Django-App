1f75b3f79f33a05e65eefc03a403ad2df0217585
Vinodh <vinodhtandrothu@gmail.com>
2023-02-18 18:04:05 -0600

    installing psycopg2

--- storefront/settings/devlop.py
+++ storefront/settings/devlop.py
@@ -10,7 +10,7 @@ SECRET_KEY = 'django-insecure-hs6j037urx6iav+7#10%-vu4l4f5@@-1_zo)oft4g7$vf2$jmp
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': 'storefront3',
         'HOST': 'localhost',
         'USER': 'root',
